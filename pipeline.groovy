0efedb7153354311ea6dc66f8fe44fd189f424d6
Tharindu Jayawardhana <tharinduimalka915@gmail.com>
2024-04-18 11:32:10 +0530

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -6,12 +6,12 @@ pipeline {
   stages {
     stage('build'){
       steps {
-        sh 'docker build -t jenkins-node-image .'
+        sh 'docker build -t thetharz/3996-node-app-image .'
       }
     }
     stage('run'){
       steps{
-        sh 'docker run -d -p 5000:3000 jenkins-node-image'
+        sh 'docker run -d -p 5000:3000 thetharz/3996-node-app-image'
       }
     }
     stage('final'){
@@ -22,10 +22,10 @@ pipeline {
     stage('push'){
       steps{
         script{
-          withCredentials([string(credentialsId: 'docker-pwd', variable: 'docker-pwd')]) {
-            sh 'docker login -u thetharz -p ${docker-pwd}'
-          }
-            sh 'docker push thetharz/3996-node-app-image'
+          withCredentials([string(credentialsId: 'docker-pwd', variable: 'docker')]) {
+    sh 'docker login -u thetharz -p ${docker}'
+}
+sh 'docker push thetharz/3996-node-app-image'
         }
       }
     }
